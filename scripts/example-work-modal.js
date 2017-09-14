f8b26e5c2148004eb9a5d4256ea1c7caedc3fc1b
SASANKA JONNA <j.sasank@gmail.com>
2017-09-13 23:46:11 -0400

    Re-added

---
+++ scripts/example-work-modal.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class ExampleWorkModal extends React.Component {
+
+  render() {
+    let example = this.props.example;
+    let modalClass = this.props.open ? 'modal--open' : 'modal--closed';
+    let imageSrc=example.image.src;
+    imageSrc=imageSrc.replace("images/","");
+    imageSrc=imageSrc.replace(".jpg","");
+    imageSrc=imageSrc.replace(".png","");
+
+
+    return (
+      <div className={"background--skyBlue " + modalClass +" section--alignCentered"} >
+      <link rel="stylesheet" href="node_modules/react-responsive-carousel/lib/styles/carousel.min.css"/>
+      <link rel="stylesheet" href="CSS/CarouseCustom.css"/>
+
+      <span className="color--cloud modal__closeButton"
+      onClick={this.props.closeModal}>
+      <i className="fa fa-window-close-o"></i>
+      </span>
+
+
+      <h3 className="modal__title1">
+      { example.title }
+      </h3>
+      <p className="modal__description1">
+      { example.desc  }
+      </p>
+
+      <div className={imageSrc =="Certification" ? 'alignDiv' : 'section_invisible'} id="CertificationCarousel-container">tikka</div>
+
+      <div className={imageSrc =="Skills" ? 'alignDiv' : 'section_invisible'} id="SkillCarousel-container">tikka</div>
+
+
+
+
+      </div>
+
+    )
+  };
+};
+
+export default ExampleWorkModal;
---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work'
+
+const myWork = [
+  {
+    'title': "My Certifications",
+    'href': "./Certification.html",
+    'desc': "Certifications are not a cover up for lack of experience, but they are a value add and also add credibility.",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/Certification.jpg",
+      'comment': ""
+    }
+  },
+  {
+    'title': "My Skills",
+    'href': "https://example.com",
+    'desc': "Few of the technology skills that I am either learning, comfortable or good at...",
+    'image': {
+      'desc': "My Skills",
+      'src': "images/Skills.jpg",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Work Example",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'image': {
+      'desc': "example screenshot of a project involving cats",
+      'src': "images/example3.png",
+      'comment': `"Bengal cat" by roberto shabs is licensed under CC BY 2.0
+                  https://www.flickr.com/photos/37287295@N00/2540855181`
+    }
+  }
+]
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'))
